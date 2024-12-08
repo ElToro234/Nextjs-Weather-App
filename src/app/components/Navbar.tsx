711cfd908e616e6625c4eaf3abf4eb145f60b5c6
Alexander Liu <alexanderliuxinlu@gmail.com>
2024-12-08 15:55:46 -0500

    SearchBar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { MdWbSunny } from 'react-icons/md'
+import { MdMyLocation, MdOutlineLocationOn, MdWbSunny } from 'react-icons/md'
+import SearchBox from './SearchBox'
 
 type Props = {}
 
@@ -7,10 +8,21 @@ export default function Navbar({}: Props) {
     return (
         <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
             <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
-                <p className="flex items-center justify-center gap-2">
+                <div className="flex items-center justify-center gap-2">
                     <h1 className="test-gray-500 text-3xl">WEATHER</h1>
                     <MdWbSunny className="text-3xl mt-1 text-yellow-300"/>
-                </p> 
+                </div> 
+
+                <section className="flex gap-2 items-center">
+                    <MdMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer"/>
+                    <MdOutlineLocationOn className="text-3xl"/>
+                    <p className="text-slate-900/80 text-sm">canada</p>
+
+                    {/* SEARCH BOX */}
+                    <div>
+                    <SearchBox/>
+                    </div>
+                </section>
             </div>
         </nav>
     )
