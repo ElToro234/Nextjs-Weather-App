c4676c6c4a3a60762c7a988c88a094837de4c54a
Alexander Liu <alexanderliuxinlu@gmail.com>
2024-12-09 16:09:44 -0500

    weather for the next 7 days

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ const uniqueDates = [
 // Filtering the data to get first entry after 6 AM for each unique data
 const firstDataForEachDate = uniqueDates.map((date) => {
   return data?.list.find((entry) => {
-    const entryDate = new Date(entry.dt * 1000).toISOString().split("t")[0];
+    const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0];
     const entryTime = new Date(entry.dt * 1000).getHours();
     return entryDate === date && entryTime >= 6;
   })
@@ -197,8 +197,32 @@ const firstDataForEachDate = uniqueDates.map((date) => {
       {/* 7 days data */}
       <section className="flex w-full flex-col gap-4">
         <p className="text-2xl">Forecast (7 days) </p>
-
-        <ForecastWeatherDetail/>
+        {firstDataForEachDate.map((d, i) => (
+         <ForecastWeatherDetail 
+            key={i}
+            description={d?.weather[0]?.description ?? ""}
+            weatherIcon={d?.weather[0]?.icon ?? ""}
+            date=       {format (parseISO(d?.dt_txt ?? ""), "dd.MM")}
+            day=        {format (parseISO(d?.dt_txt ?? ""), "EEEE")}
+            feels_like= {d?.main.feels_like ?? 0}
+            temp=       {d?.main.temp ?? 0}
+            temp_max=   {d?.main.temp_max ?? 0}
+            temp_min=   {d?.main.temp_min ?? 0}
+            airPressure={`${d?.main.pressure} hPa`}
+            humidity=   {`${d?.main.humidity}%`}
+            sunrise={format(
+              fromUnixTime(data?.city.sunrise ?? 1702517657),
+              "H:mm"
+            )}
+            sunset={format(
+              fromUnixTime(data?.city.sunset ?? 1702517657),
+              "H:mm"
+            )}
+            visability={`${metersToKm(d?.visibility ?? 10000)}`}
+            windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)}`}
+         />
+        ))}
+       
       </section>
     </main>
   </div> 
