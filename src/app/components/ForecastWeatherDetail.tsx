72f56d2db656d81f2be5c7ad297699cd538fe3b1
Alexander Liu <alexanderliuxinlu@gmail.com>
2024-12-09 14:26:55 -0500

    WeatherDetails

---
+++ src/app/components/ForecastWeatherDetail.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Container from './Container'
+
+export default function ForecastWeatherDetail(props: React.HTMLProps<HTMLDivElement>){
+    return (
+        <Container></Container>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { format, parseISO } from "date-fns";
 import Container from "./components/Container";
 import { convertKelvinToCelsius } from "./utils/convertKelvinToCelsius";
 import WeatherIcon from "./components/WeatherIcon";
+import { getDayOrNightIcon } from "./utils/getDayOrNightIcon";
 
 interface WeatherData {
   cod: string;
@@ -137,18 +138,33 @@ const firstData = data?.list[0];
                   font-semibold"
                 >
                   <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), "h:mm a")}</p>
-                  <WeatherIcon iconName={d.weather[0].icon}/>
+                  
+                  {/* <WeatherIcon iconName={d.weather[0].icon}/> */}
+                  <WeatherIcon iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)}/>
                   <p >{convertKelvinToCelsius(d?.main.temp ?? 0)}</p>
                 </div>
               )}
             </div>
           </Container>
         </div>
+        <div className="flex gap-4">
+          {/* left */}
+          <Container className="w-fit justify-center flex-col px-4 items-center">
+            <p>{firstData?.weather[0].description}</p>
+            <WeatherIcon iconName={getDayOrNightIcon(
+              firstData?.weather[0].icon ?? "",
+              firstData?.dt_txt ?? "")}/>
+          </Container>
+          
+          <Container className="bg-yellow-300/80 px-6 gap-4 justify-between
+          overflow-x-auto"></Container>
+          {/* right */}
+        </div>
       </section>
 
       {/* 7 days data */}
-      <section>
-        
+      <section className="flex w-full flex-col gap-4">
+        <p className="text-2xl">Forecast (7 days) </p>
       </section>
     </main>
   </div> 
